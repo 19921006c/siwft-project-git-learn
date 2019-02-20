515a762e39ac3a77f2327a8e0e66458c3271b1ce
張志豪 <dummy@g.softbank.co.jp>
2019-02-20 16:04:11 +0900

    creat project

---
+++ SwiftProGit/SwiftProGit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftProGit
+//
+//  Created by SB on 2019-02-20.
+//  Copyright © 2019 com.chou.jp. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
